926ec104e228ff249dd33941551f77961e58f891
Aybars Arslan <arslanaybars@gmail.com>
2015-03-02 15:46:11 +0200

    ui01MaterialDesign v1

--- Ui01MaterialDesign/app/src/main/java/aybars/arslan/ui01materialdesign/SubActivity.java
+++ Ui01MaterialDesign/app/src/main/java/aybars/arslan/ui01materialdesign/SubActivity.java
@@ -1,34 +1,33 @@
 package aybars.arslan.ui01materialdesign;
 
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    private Toolbar toolbar;
+public class SubActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
 
-        toolbar= (Toolbar) findViewById(R.id.app_bar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
-
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_sub, menu);
         return true;
     }
 
@@ -41,11 +40,11 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Toast.makeText(this,"Hey you ! "+item.getTitle(),Toast.LENGTH_SHORT).show();
             return true;
         }
-        if (id == R.id.navigate) {
-            startActivity(new Intent(this, SubActivity.class));
+
+        if (id == android.R.id.home ) {
+            NavUtils.navigateUpFromSameTask(this);
         }
 
         return super.onOptionsItemSelected(item);
